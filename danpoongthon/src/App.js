6ffebf6f3aee6e12a2cf7dbd70a530104f57a5a3
seomiii <rlaaltj1765@gmail.com>
2022-11-15 15:34:43 +0900

    base by miseo

--- danpoongthon/src/App.js
+++ danpoongthon/src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Home from './component/Home';
+import Main from './component/Main';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          {/* 시작페이지 */}
+          <Route exact path="/" element={<Home />}/>
+
+          {/* 메인페이지 */}
+          <Route exact path="main" element={<Main />}/>
+
+
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ danpoongthon/src/component/Main.js
@@ -0,0 +1,12 @@
+import {useLocation, Link} from 'react-router-dom';
+
+function Main(){
+    console.log('main');
+    return(
+        <div>
+            메인 페이지 입니다
+        </div>
+    )
+}
+
+export default Main;
